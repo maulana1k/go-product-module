43d2e1385410d5b8de1b41ccc5acef236957eeee
Maulana <mm930199@gmail.com>
2022-02-04 13:16:58 +0700

    add product

--- product.go
+++ product.go
@@ -1,12 +1,15 @@
 package product
 
 type Product struct {
-	name, brand, variant string
-	stock, price         int
+	name, brand  string
+	stock, price int
 }
 
-var ProductStorage []Product
+var ProductStorage = make([]Product, 0, 20)
 
 func GetProduct() []Product {
+	var product1 = Product{"laptop", "asus", 12, 8000000}
+	ProductStorage = append(ProductStorage, product1)
+
 	return ProductStorage
 }
